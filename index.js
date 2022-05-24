d59c925384167a4b5d7032ef1204200a9ce37dc5
rahathossain3 <rahathossain210@gmail.com>
2022-05-24 16:12:13 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 // dotenv config
 require('dotenv').config();
-
+//mongodb
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -12,6 +13,48 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.of0kn.mongodb.net/?retryWrites=true&w=majority`;
+
+//Client
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        //products collections
+        const productCollection = client.db('pixel_camera').collection('products');
+
+        //get all products
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+
+            res.send(products)
+        })
+
+
+
+
+
+
+
+    }
+    finally {
+
+    }
+
+
+}
+
+run().catch(console.dir);
+
+
+
+
 
 
 app.get('/', (req, res) => {
