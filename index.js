824b95f01b0dda0246cf1ed0885ca1c215612a28
rahathossain3 <rahathossain210@gmail.com>
2022-05-27 16:34:15 +0600

    single product add

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 // dotenv config
 require('dotenv').config();
 //mongodb
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -39,6 +39,13 @@ async function run() {
             res.send(products)
         })
 
+        // get single product 
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await productCollection.findOne(query);
+            res.send(item);
+        })
 
 
 
